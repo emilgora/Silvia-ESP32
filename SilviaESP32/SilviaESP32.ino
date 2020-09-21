8019d09817b7c08b08547595b62869aa24ec447c
emilgora <30513364+emilgora@users.noreply.github.com>
2020-09-21 13:20:37 -0400

    Update SilviaESP32.ino

--- SilviaESP32/SilviaESP32.ino
+++ SilviaESP32/SilviaESP32.ino
@@ -1,42 +1,37 @@
-#include <ETH.h>
-#include <WiFi.h>
-#include <WiFiAP.h>
-#include <WiFiClient.h>
-#include <WiFiGeneric.h>
-#include <WiFiMulti.h>
-#include <WiFiScan.h>
-#include <WiFiServer.h>
-#include <WiFiSTA.h>
-#include <WiFiType.h>
-#include <WiFiUdp.h>
-
-WiFiServer server(80);
-
-#include <MiniPID.h>
-
 #include <Arduino.h>
 
-#include <Adafruit_MAX31865.h>
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WiFiServer.h>
+WiFiServer server(80);
 
-#include <U8x8lib.h>
+#include <U8x8lib.h> //https://github.com/olikraus/u8g2/wiki
 #ifdef U8X8_HAVE_HW_SPI
 #include <SPI.h>
 #endif
-U8X8_SSD1306_128X64_NONAME_SW_I2C u8x8(/* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);   // OLEDs without Reset of the Display
+U8X8_SSD1306_128X64_NONAME_SW_I2C u8x8(/* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);   // OLEDs without Reset of the Display (change config if applicable)
+
+#include <MiniPID.h> //https://github.com/tekdemo/MiniPID
+double p = 1;
+double i = 0;
+double d = 0;
+MiniPID pid = MiniPID(p,i,d);
+//set any other PID configuration options here. 
+
+#include <Adafruit_MAX31865.h> //https://github.com/adafruit/Adafruit_MAX31865
+Adafruit_MAX31865 thermo = Adafruit_MAX31865(10, 11, 12, 13);
 
 int fooBar = 0;
 
-const int guiLeftPin = 34;
-const int guiRightPin = 39;
-const int guiSelectPin = 36;
+double sensor = 0; //hardware defined value (in this case from PT100 RTD Sensor attached to boiler)
+double targetTemp = 0; //user defined value (in this case from web page)
+
+double output = 0; //post processed value
 
 void setup(void)
 {
+  Serial.begin(9600);
   
-  pinMode(guiLeftPin, INPUT);
-  pinMode(guiRightPin, INPUT);
-  pinMode(guiSelectPin, INPUT);
-
   u8x8.begin();
   u8x8.setPowerSave(0);
 
@@ -45,15 +40,21 @@ void setup(void)
   u8x8.drawString(2,5,"please wait.");
   delay(5000);
   u8x8.clear();
-  
+
 }
 
 void loop(void)
 {
 
-
 // 3x6 font for set temp read + weather icon
-// 
+
+  //get sensor value
+  //set temperature target value
+  double output=pid.getOutput(sensor,targetTemp);
+  //do something with the output
+  Serial.println(output);
+  delay(50);
+  
 
   
 }
